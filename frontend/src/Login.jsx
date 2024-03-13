7b853f0d7f58bcf0d3d59783f9adfdea2f5da7d5
AryanRanderiyaWorkstation <aryan.k.randeriya@gmail.com>
2024-03-13 15:08:11 +0530

    Updated User Interface, added icons & fixed Select

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,25 +1,30 @@
 import { Input } from "@nextui-org/react";
-import { Button, ButtonGroup } from "@nextui-org/react";
-import { Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
+import { EyeSlashFilledIcon, EyeFilledIcon } from "./icons";
+import * as React from "react";
 
 export function SelectComponent() {
+  const loginTypes = [
+    { label: "Faculty", value: "Faculty" },
+    { label: "Head of Department", value: "HOD" },
+    { label: "Director", value: "Director" },
+  ];
+
   return (
     <>
-      <Select label="Select Login Type" className="max-w-xs">
-        {animals.map((animal) => (
-          <SelectItem key={animal.value} value={animal.value}>
-            {animal.label}
-          </SelectItem>
-        ))}
-      </Select>
       <Select
-        label="Favorite Animal"
-        placeholder="Select an animal"
+        label="User Type"
+        placeholder="Select User Type"
         className="max-w-xs"
+        value="Faculty"
+        isRequired={"true"}
+        defaultSelectedKeys={["Faculty"]}
+        variant="faded"
       >
-        {animals.map((animal) => (
-          <SelectItem key={animal.value} value={animal.value}>
-            {animal.label}
+        {loginTypes.map((loginType) => (
+          <SelectItem key={loginType.value} value={loginType.value}>
+            {loginType.label}
           </SelectItem>
         ))}
       </Select>
@@ -27,11 +32,51 @@ export function SelectComponent() {
   );
 }
 
+export function Inputs() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  return (
+    <>
+      <Input
+        variant="faded"
+        isRequired
+        type="email"
+        label="Email"
+        className="max-w-xs"
+        isClearable="true"
+      />
+      <Input
+        variant="faded"
+        label="Password"
+        placeholder="Enter your password"
+        endContent={
+          <button
+            className="focus:outline-none"
+            type="button"
+            onClick={toggleVisibility}
+          >
+            {isVisible ? (
+              <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            ) : (
+              <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            )}
+          </button>
+        }
+        type={isVisible ? "text" : "password"}
+        className="max-w-xs"
+      />
+    </>
+  );
+}
+
 export default function Login() {
   return (
     <>
-      <Input isRequired type="email" label="Email" className="max-w-xs" />
-      <Input isRequired type="password" label="Password" className="max-w-xs" />
+      <SelectComponent />
+
+      <Inputs />
       <Button color="primary">Login</Button>
     </>
   );
