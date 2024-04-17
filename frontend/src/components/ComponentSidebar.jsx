a14b62a122f6281312cc1847f7b1a68c5628e83e
AryanRanderiyaWorkstation <aryan.k.randeriya@gmail.com>
2024-04-18 00:00:46 +0530

    update ComponentSidebar.jsx

--- frontend/src/components/ComponentSidebar.jsx
+++ frontend/src/components/ComponentSidebar.jsx
@@ -28,7 +28,7 @@ export default function SideBar() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://pdeu-research-portal-api.vercel.app//userinfo",
+          "https://pdeu-research-portal-api.vercel.app/userinfo",
           {
             method: "POST",
             headers: {
